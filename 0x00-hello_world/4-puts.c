4b097790f13a306ba90be672f6fa74cff8a8e84f
James Tosatti <45105717+tosattijames@users.noreply.github.com>
2019-02-10 18:08:25 -0500

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
-#!/bin/bash
 #include <stdio.h>
 
-int main()
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle\n");
-
-    return 0;
+    
+    return (0);
 }
