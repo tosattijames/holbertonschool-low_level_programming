e7f4440903d5f5de3d7444a60e969d3461251ebd
James Tosatti <45105717+tosattijames@users.noreply.github.com>
2019-02-10 23:54:16 -0500

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,24 +1,10 @@
 #include <stdio.h>
-<<<<<<< HEAD
-/**
-* main - output string eith puts command.
-*
-*Return: Always 0 (Success)
-*/
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-=======
-
 /**
 *puts output
 *
 */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    
-    return (0);
->>>>>>> ea7112e060120c75f579878416712d201863e6d7
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
