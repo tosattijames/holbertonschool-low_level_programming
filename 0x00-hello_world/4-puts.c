30feded4079a2a83e12d42aa021d4174ab52dfa3
tosattijames <703@holbertonschool.com>
2019-02-11 05:22:11 +0000

    main - Prints output using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,24 +1,11 @@
 #include <stdio.h>
-<<<<<<< HEAD
 /**
-* main - output string eith puts command.
+* main - Entry point for all functions
 *
-*Return: Always 0 (Success)
+* Return: Always 0
 */
-int main(void)
+int main(void) /
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-=======
-
-/**
-*puts output
-*
-*/
-int main(void)
-{
-    puts("\"Programming is like building a multilingual puzzle");
-    
-    return (0);
->>>>>>> ea7112e060120c75f579878416712d201863e6d7
 }
