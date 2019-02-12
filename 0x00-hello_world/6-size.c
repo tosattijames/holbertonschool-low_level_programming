bbe23b3b87b9348d728bb1f81b90d8f31de81753
tosattijames <703@holbertonschool.com>
2019-02-12 14:22:49 +0000

    prints sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
-#include <limits.h>
+/**
+* main - Short description, single line
+(* a blank line
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: 0
+*/
+int main(void)
+{
+printf("Size for char: %d \n", sizeof(char));
+printf("Size for int: %d \n", sizeof(int));
+printf("Size for long int: %d \n", sizeof(long int));
+printf("Size for long long int: %d \n", sizeof(long long int));
+printf("Size for float: %d \n", sizeof(float));
 
-int main() {
-printf("Size for char : %d \n", sizeof(char));
-printf("Size for int : %d \n", sizeof(int));
-return 0;
+return (0);
 }
