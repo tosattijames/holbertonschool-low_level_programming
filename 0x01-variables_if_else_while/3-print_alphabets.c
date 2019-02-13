cb54cafda7c75c18970cf562b1a4a527710b38da
tosattijames <703@holbertonschool.com>
2019-02-13 01:30:04 +0000

    source

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Print the lowercase alphabet.
+*
+* Description:The program uses putchar
+* function to print a lowercase alphabet
+* excluding q abd e followed by a new line.
+*
+* Return: 0
+*/
+int main(void)
+{
+
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+		putchar(ch);
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+		putchar(ch);
+	putchar('\n');
+return (0);
+}
