571e710604e1d69b7d87e4e215f9711013e27553
James Tosatti <tosattijames@gmail.com>
2019-02-17 03:40:48 +0000

    source

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -10,22 +10,22 @@
  */
 int main(void)
 {
-  int a;
-  int b;
+int a;
+int b;
 
 for (a = '0' ; a <= '9' ; a++)
-  {
-    for (b = '0' ; b <= '9' ; b++)
-      {
-	putchar(a);
-	putchar(b);
-	if (a < '9' || b < '9')
-	  {
-	    putchar(',');
-	    putchar(' ');
-	  }
-      }
-  }
+{
+for (b = '0' ; b <= '9' ; b++)
+{
+putchar(a);
+putchar(b);
+if (a < '9' || b < '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
 putchar('\n');
 return (0);
 }
