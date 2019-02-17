8bf0b675f79e2ff935e144711b8fa58d7bdf2a00
James Tosatti <tosattijames@gmail.com>
2019-02-17 03:30:27 +0000

    source

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ *main - evaluate if the random integer
+ *is 0, postive, or negative
+ *
+ *Description: evaluate if the random integer is a postive
+ *number, negative number, or egual to zero.
+ *
+ *Return:0
+ */
+int main(void)
+{
+  int a;
+  int b;
+
+for (a = '0' ; a <= '9' ; a++)
+  {
+    for (b = '0' ; b <= '9' ; b++)
+      {
+	putchar(a);
+	putchar(b);
+	if (a < '9' || b < '9')
+	  {
+	    putchar(',');
+	    putchar(' ');
+	  }
+      }
+  }
+putchar('\n');
+return (0);
+}
