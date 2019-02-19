eeb62cc5904f9e1decfa03a34bbe512afb587e3e
James Tosatti <tosattijames@gmail.com>
2019-02-19 15:14:34 +0000

    source

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * main - Print lowercase alphabet
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char c;
+
+c = 'a';
+
+while
+(c <= 'z');
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
