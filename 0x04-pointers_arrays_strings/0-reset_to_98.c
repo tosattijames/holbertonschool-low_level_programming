c00a8a867f254cf67050ea7d1c60b6fc39f67ea9
James Tosatti <45105717+tosattijames@users.noreply.github.com>
2019-02-19 21:57:28 -0500

    Update 0-reset_to_98.c

--- 0x04-pointers_arrays_strings/0-reset_to_98.c
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
-* reset_to_98 - Reset the interger using pointers.
-*@n: pointer passed to function
-* * Return: void  
+*  reset_to_98 - Reset the interger using pointers.
+*  @n: pointer passed to function
+*  Return: void  
 */
 void reset_to_98(int *n)
 {
