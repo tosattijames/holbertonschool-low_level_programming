1e39486692ee8f3027f664e1150bf3a9458a60a5
James Tosatti <tosattijames@gmail.com>
2019-02-20 02:47:10 +0000

    custom header

---
+++ 0x04-pointers_arrays_strings/holberton.h
@@ -0,0 +1,33 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(int c); 
+
+/* 0x02*/
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+
+/*0x03*/
+
+/*0x04*/
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+#endif /* HOLBERTON_H */
