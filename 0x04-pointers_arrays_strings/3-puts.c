0d15eebef0d80a97dbfd391ee5407d50110c5973
James Tosatti <tosattijames@gmail.com>
2019-02-21 03:07:09 +0000

    source

---
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* _puts - prints a string
+* @str: string to print
+* Return: print string to output
+*/
+void _puts(char *str)
+{
+while (*str  != '\0')
+{
+_putchar(*str);
+ str++;
+}
+_putchar('\n');
+}
