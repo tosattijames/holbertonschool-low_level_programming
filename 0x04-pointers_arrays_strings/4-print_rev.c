5892a305757de735a7f90ea29da37f690a04dc1a
James Tosatti <tosattijames@gmail.com>
2019-02-21 05:26:00 +0000

    source

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to print
+ */
+void print_rev(char *s)
+{
+
+int x = 0;
+
+while (s[x] != 0)
+x++;
+x = x - 1;
+while (x >= 0)
+{
+_putchar(s[x]);
+}
+_putchar('\n');
+}
