3907b34ead868549235af58d8ef73d6bf33bc20c
James Tosatti <tosattijames@gmail.com>
2019-02-24 16:43:32 +0000

    Added ascii codes

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 
 /**
-* _int _isalpha - Checks if the passed character is alphabetic.
-* @c: letter to be checked
-*
-* Return: 1 = alphabetic, 0 = non-alphabetic
-*/
-
+ * _isalpha - Checks if the passed character is alphabetic.
+ *
+ * @c: character to be checked
+ *
+ * Return: 1 = alphabetic, 0 = non-alphabetic
+ */
 int _isalpha(int c)
 {
-  if (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+if (c >= 65 && c <= 122)
 {
 return (1);
 }
