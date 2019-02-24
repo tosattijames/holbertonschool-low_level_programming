2cda49642b1b2884d7bf84184a1694d62168ed5a
James Tosatti <tosattijames@gmail.com>
2019-02-24 20:08:13 +0000

    source

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _abs - omputes the absolute value of an integer
+ * @n: input number
+ *
+ * Return: n = absolute value, 0 = zero
+ */
+
+int _abs(int n)
+{
+if (n > 0)
+{
+return (n);
+}
+else if (n < 0)
+{
+return (n * -1);
+}
+else
+{
+return (0);
+}
+}
