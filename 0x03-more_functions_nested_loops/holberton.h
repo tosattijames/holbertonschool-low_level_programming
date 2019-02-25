a81c4ec86e469c1dbeaaeb844cf730f5e67d0ecf
James Tosatti <tosattijames@gmail.com>
2019-02-25 05:30:23 +0000

    prototype

---
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -0,0 +1,27 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(int c); /* 00- function prototype for _putchar */
+void print_alphabet(void); /* 01- function prototype for print_alphabet */
+void print_alphabet_x10(void); /* 02- function prototype for print_alphabet_0x */
+int _islower(int c);/* 03- function prototype for _islower */
+int _isalpha(int c);/* 04- function prototype for _isalpha */
+int print_sign(int n);/* 05- function prototype for print_sign */
+int _abs(int);/* 06- function prototype for _abs */
+int print_last_digit(int);/*07- function prototype for print_last_digit */
+void jack_bauer(void);/* 08- function prototype for jack_bauer */
+void times_table(void);/* 09- function prototype for times_tablea */
+int add(int, int);/* 10- function prototype for add */
+void print_to_98(int n);/* 11- function prototype for print_to_98 */
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif /* HOLBERTON_H */
