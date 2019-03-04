58cb47b6d1d61bbaa5961e3fccae9cb8f13ae9ad
James Tosatti <tosattijames@gmail.com>
2019-03-04 02:44:51 +0000

    source

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ * Return: Always 0.
+ */
+
+void times_table(void)
+{
+int x, y;
+
+x = 0;
+while (x < 9)
+{
+y = 0;
+while (y <= 9)
+{
+if ((x *y) > 9)
+{
+_putchar(' ');
+_putchar(((x *y) / 10) + '0');
+_putchar(((x *y) % 10) + '0');
+}
+else
+{
+if (y != 0)
+{
+_putchar(' ');
+_putchar(' ');
+}
+_putchar((x *y) +'0');
+}
+if (y != 9)
+_putchar(',');
+y++;
+}
+_putchar('\n');
+x++;
+}
+}
