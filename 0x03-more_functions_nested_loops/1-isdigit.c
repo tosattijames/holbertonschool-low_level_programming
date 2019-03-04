db5615c4a8a15ebce09a27878082dc0d510efba2
James Tosatti <tosattijames@gmail.com>
2019-03-04 06:10:06 +0000

    source

---
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9
+ *
+ *@c: character input
+ *
+ * Return: 1 if true, 0 false
+ */
+int _isdigit(int c)
+{
+if
+((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+return (0);
+}
+else if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+return (0);
+}
