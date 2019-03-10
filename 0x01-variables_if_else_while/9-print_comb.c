e9274a1c9eb0cd8761a3a52185d77faba6ef82a5
James Tosatti <tosattijames@gmail.com>
2019-03-10 18:53:39 +0000

    updated formatting

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,30 +1,28 @@
 #include <stdio.h>
+
 /**
-* main - Print the lowercase alphabet.
-*
-* Description:The program uses putchar
-* function to print a lowercase alphabet
-* excluding q abd e followed by a new line.
-*
-* Return: 0
-*/
+ * main - prints 1 - 9
+ *
+ * Description - print numbers 1 -9 separated by ,, followed by a space
+ *               in ascending order. Decimal values are used for th ','
+ *               and 'blanke space'.
+ * Return: 0
+ */
+
 int main(void)
 {
+int num1;
+int comma1 = 32;
+int space1 = 44;
 
-int n;
-int b = 32;
-int c = 44;
-int d = 36;
-
-for (n = '0' ; n <= '9' ; n++)
+for (num1 = '0' ; num1 <= '9' ; num1++)
 {
-putchar(n);
-if (n != '9')
+putchar(num1);
+if (num1 != '9')
 {
-putchar(c);
-putchar(b);
+putchar(space1);
+putchar(comma1);
 }
 }
-putchar(d);
 return (0);
 }
