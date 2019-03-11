b105551b3c06d284b96dd6580cbbcb9f0c28e5c6
James Tosatti <tosattijames@gmail.com>
2019-03-11 01:40:13 +0000

    updated source

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,29 +1,39 @@
 #include "holberton.h"
-
 /**
- * jack_bauer - print every minute from 00:00 to23:59.
+ * jack_bauer - prints starting from 00:00 to 23:59
  *
- * Return: 0 = zero
- */
-
+ * Return: 0.
+ *
+ *
+*/
 void jack_bauer(void)
 {
-char num1, num2, num3, num4;
+int num1;
+int num2;
+int num3;
+int num4;
 
-for (num1 = '0' ; num1 <= '2' ; num1++)
+for (num1 = '0' ; num1 <= '2' ; num1++) /** Digit One Loop */
 {
-for (num2 = '0' ; num2 <= '3' ; num2++)
+for (num2 = '0' ; num2 <= '4' ; num2++) /** Digit Two Loop */
 {
-for (num3 = '0' ; num3 <= '5' ; num3++)
+for (num3 = '0' ; num3 <= '5' ; num3++) /** Digit Three Loop */
 {
-for (num4 = '0' ; num4 <= '9' ; num4++)
+for (num4 = '0' ; num4 <= '9' ; num4++) /** Digit Four Loop */
 {
-_putchar(num1);
+_putchar(num1); /** Formating Agruements */
 _putchar(num2);
 _putchar(':');
 _putchar(num3);
 _putchar(num4);
+if ((num1 == '2' && num2 == '3') && (num3 == '5' && num4 == '9'))
+{
 _putchar('\n');
+}
+else
+{
+_putchar('\n');
+}
 }
 }
 }
