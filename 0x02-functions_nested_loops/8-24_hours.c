e9a2843d2b19b7e7c511caf0e64d77663048dcf1
James Tosatti <tosattijames@gmail.com>
2019-03-11 03:31:34 +0000

    source

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * jack_bauer - prints starting from 00:00 to 23:59
+ * jack_bauer - prints from 00:00 to 23:59
  *
  * Return: 0.
  *
@@ -8,34 +8,29 @@
 */
 void jack_bauer(void)
 {
-int num1;
-int num2;
-int num3;
-int num4;
-
-for (num1 = '0' ; num1 <= '2' ; num1++) /** Digit One Loop */
+int num1, num2, num3, num4;
+/** Digit One Loop */
+for (num1 = '0' ; num1 <= '2' ; num1++)
 {
-for (num2 = '0' ; num2 <= '4' ; num2++) /** Digit Two Loop */
+/** Digit Two Loop */
+for (num2 = '0' ; num2 <= '4' ; num2++)
 {
-for (num3 = '0' ; num3 <= '5' ; num3++) /** Digit Three Loop */
+/** Digit Three Loop */
+for (num3 = '0' ; num3 <= '5' ; num3++)
 {
-for (num4 = '0' ; num4 <= '9' ; num4++) /** Digit Four Loop */
+/** Digit Three Loop */
+for (num4 = '0' ; num4 <= '9' ; num4++)
 {
-_putchar(num1); /** Formating Agruements */
+/** Printing Agruements */
+_putchar(num1);
 _putchar(num2);
 _putchar(':');
 _putchar(num3);
 _putchar(num4);
-if ((num1 == '2' && num2 == '3') && (num3 == '5' && num4 == '9'))
-{
-_putchar('\n');
-}
-else
-{
 _putchar('\n');
 }
 }
 }
 }
-}
+
 }
