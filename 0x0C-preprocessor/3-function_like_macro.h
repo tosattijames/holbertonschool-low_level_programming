739889b56c1e4fd3217ebac96b1587040087119b
James Tosatti <tosattijames@gmail.com>
2019-03-14 01:17:28 +0000

    Added Ternary Operator

---
+++ 0x0C-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef _3_FUNCTION_LIKE_MACRO_H
+#define _3_FUNCTION_LIKE_MACRO_H
+
+/**
+ * ABS  - Computes the absolute value of a number x
+ *@x: input number
+ *
+ */
+
+#define ABS(x) (x < 0 ? -x : x)
+#endif
