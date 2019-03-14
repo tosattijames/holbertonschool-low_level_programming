b586a92656b4e094fa4b6945a4fafc7cb5fb1b31
James Tosatti <tosattijames@gmail.com>
2019-03-14 00:17:26 +0000

    source

--- 0x0C-preprocessor/2-main.c
+++ 0x0C-preprocessor/2-main.c
@@ -1,5 +1,19 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-#define _FILE_
-printf("%s\n", _FILE_)
+
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main -  prints the name of the file it
+ * it was compiled from, followed by a new line.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+printf("%s\n", __BASE_FILE__);
+return (0);
+}
 #endif
