26ab426992fa19348a705ec765204e8eaacf334c
tosattijames <703@holbertonschool.com>
2019-03-18 23:05:11 +0000

    source

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Passes string to be printed
+ * @name: Pointer to the strings to be printed
+ * @f: Fuction pointer
+ * Description: Print_name takes a string pointer
+ * of char type then passes the string into the
+ * calling function to be printer
+ * Return: Void
+*/
+
+
+
+void print_name(char *name, void (*f)(char *))
+{
+if (name == NULL)
+return;
+/** no input */
+if (f == NULL)
+return;
+/** no input */
+f(name);
+/** pass name varible to function */
+}
