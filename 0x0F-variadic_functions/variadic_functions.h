70adafb46d41bb260433a8dec0643e2e485dceda
tosattijames <703@holbertonschool.com>
2019-03-19 18:40:29 +0000

    added typedef

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,7 +1,14 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+typedef struct op
+{
+char *op;
+void (*f)();
+} op_t;
+
 int _putchar(int c);
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
