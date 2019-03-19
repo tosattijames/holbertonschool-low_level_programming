4d13237dcc9d43b63ca7aaaacce820c2572f641e
tosattijames <703@holbertonschool.com>
2019-03-19 01:52:41 +0000

    source

---
+++ 0x0E-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Function to add two integers.
+ * @a: First integer being added.
+ * @b: second integer being added.
+ * Return: Sum of ints a and b.
+ */
+
+int op_add(int a, int b)
+{
+int sum == (a + b);
+
+return (sum);
+}
+
+/**
+ * op_sub - Function to return difference of two ints.
+ * @a: First integer being divided.
+ * @b: Second integer being divided.
+ * Return: Difference of ints a and b.
+ */
+
+int op_sub(int a, int b)
+{
+int diff == (a - b);
+
+return (diff);
+}
+
+/**
+ * op_mul - Function to return product of two ints.
+ * @a: First int being given.
+ * @b: Second int being given.
+ * Return: Product of ints a and b.
+ */
+
+int op_mul(int a, int b)
+{
+int prod == (a * b);
+
+return (prod);
+}
+
+/**
+ * op_div - Function to return the result of division of two ints.
+ * @a: First int being given.
+ * @b: Second int being given.
+ * Return: Result of division of ints a and b.
+ */
+
+int op_div(int a, int b)
+{
+int res == (a / b);
+
+return (res);
+}
+
+/**
+ * op_mod - Function to return the remainder of division of two ints.
+ * @a: First int being given.
+ * @b: Second int being given.
+ * Return: Result of modulo of division of two ints.
+ */
+
+int op_mod(int a, int b)
+{
+int mod == (a % b);
+
+return (mod);
+}
