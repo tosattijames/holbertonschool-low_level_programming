06bc7750fa1baff99f290c4810939d95aada49ef
tosattijames <703@holbertonschool.com>
2019-03-22 00:46:45 +0000

    list lenght

---
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -0,0 +1,27 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements in list_t
+ * @h: pointer to head of the linked list
+ * Return: the number nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+int i = 0;
+const list_t *list = h;
+
+while (list != NULL)
+{
+if (list->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+list = list->next;
+i++;
+}
+return (i);
+}
