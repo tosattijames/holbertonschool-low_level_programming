21bff5765083f987f470adff16f00fbb21e1abd5
tosattijames <703@holbertonschool.com>
2019-03-22 07:25:19 +0000

    Free list

--- 0x11-singly_linked_lists/4-free_list.c
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -6,12 +6,21 @@
 /**
  * free_list - adds a new node at the end of a list_t list
  * @head: - beginning of list
- * Return: Return: the address of the new element, or NULL if it failed
+ * Return: Void
  */
 
 
 
 void free_list(list_t *head)
 {
+list_t *ptr;
+
+while (head != NULL)
+{
+ptr = head;
+head = head->next;
+free(ptr->str);
+free(ptr);
+}
 
 }
