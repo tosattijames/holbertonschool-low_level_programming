4b9ed94ec6cde403e5fb3b60cfbca5d8d1e5ae1d
tosattijames <703@holbertonschool.com>
2019-03-22 07:20:23 +0000

    add to end of linked list

--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -1,35 +1,56 @@
-#include "lists.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "lists.h"
 
 /**
- * add_node_end - adds a new node at the end of a list_t list
+ * add_node_end - adds a new node at the end of a linked list
  * @str: pointer to string
- * @head: - beginning of list
- * Return: Return: the address of the new element, or NULL if it failed
+ * @head: - beginning oflinked list
+ * Return: The address of the new element, or NULL if it failed
  */
+
 
 list_t *add_node_end(list_t **head, const char *str)
 {
 unsigned int i;
-list_t *new;
+list_t *new_node;
+list_t *end_node;
 
-new = malloc(sizeof(list_t));
+new_node = malloc(sizeof(list_t));
+end_node = *head;
 
-if (new == NULL)
-return (NULL);
-new->str = strdup(str);
-
-if (new->str != '\0')
+if (new_node == NULL)
 {
-for (i = 0; new->str[i] != '\0'; i++)
+return (NULL);
+}
+
+new_node->str = strdup(str);
+if (new_node->str != '\0')
+{
+for (i = 0; new_node->str[i] != '\0'; i++)
 {
 ;
 }
 }
-new->len = i;
-new->next = NULL;
-*head = new;
-return (new);
+
+
+new_node->len = i;
+
+if (*head == NULL)
+{
+*head = new_node;
+return (new_node);
+}
+else
+{
+end_node = *head;
+while (end_node->next != NULL)
+{
+end_node = end_node->next;
+}
+
+end_node->next = new_node;
+}
+return (end_node);
 }
