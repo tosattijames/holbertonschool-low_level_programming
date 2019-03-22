ba08f2576073be77374199a75331d4f3ad79a8cf
tosattijames <703@holbertonschool.com>
2019-03-22 01:07:48 +0000

    List length

--- 0x11-singly_linked_lists/1-list_len.c
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -1,25 +1,20 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 #include "lists.h"
 
 /**
- * print_list - prints all the elements in list_t
+ * list_len  - prints the nember of  elements
  * @h: pointer to head of the linked list
  * Return: the number nodes
  */
 
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 int i = 0;
 const list_t *list = h;
 
 while (list != NULL)
 {
-if (list->str == NULL)
-{
-printf("[0] (nil)\n");
-}
 list = list->next;
 i++;
 }
