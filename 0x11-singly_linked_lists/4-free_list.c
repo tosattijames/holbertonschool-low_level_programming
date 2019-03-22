f2d170f7a93df0babadba75debd9209a9f14cef6
tosattijames <703@holbertonschool.com>
2019-03-22 03:47:33 +0000

    Free list

---
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * free_list - adds a new node at the end of a list_t list
+ * @head: - beginning of list
+ * Return: Return: the address of the new element, or NULL if it failed
+ */
+
+
+
+void free_list(list_t *head)
+{
+
+}
