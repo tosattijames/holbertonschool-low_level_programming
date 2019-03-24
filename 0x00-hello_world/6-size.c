f8e5455897cd0f5b2993409c0de240893005fdb4
tosattijames <703@holbertonschool.com>
2019-03-24 20:53:00 +0000

    fixed output

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 /**
-* main - to print all value types
-*
-* Description - to print all value types
-*
-* Return: 0
-*/
+ * main - to print all value types
+ *
+ * Description: print all data types using sizeof function
+ * Return: 0
+ */
 int main(void)
 {
-printf("Size for char: %d byte(s)\n", sizeof(char));
-printf("Size for int: %d byte(s)\n", sizeof(int));
-printf("Size for long int: %d byte(s)\n", sizeof(long int));
-printf("Size for long long int: %d byte(s)\n", sizeof(long long int));
-printf("Size for float: %d byte(s)\n", sizeof(float));
-
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
