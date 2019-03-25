b9c602a17367ff919cc835ce91e80a49c4c06c60
tosattijames <703@holbertonschool.com>
2019-03-25 17:14:55 +0000

    print all elements

---
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements in list_t
+ * @h: ointer to head of the linked list
+ * Return: the number nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+	const listint_t *list = h;
+
+	while (list != NULL)
+	{
+		printf("%d\n",list->n);
+		list = list->next; /*iterate */
+		i++;
+	}
+	return (i);
+}
