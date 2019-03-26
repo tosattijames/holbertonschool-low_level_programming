b3fa65c169ae2f54cef7076d78029a5c62c7937f
tosattijames <703@holbertonschool.com>
2019-03-26 15:15:21 +0000

    print clock timer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,29 +8,18 @@
 */
 void jack_bauer(void)
 {
-int num1, num2, num3, num4;
-/** Digit One Loop */
-for (num1 = '0' ; num1 <= '2' ; num1++)
-{
-/** Digit Two Loop */
-for (num2 = '0' ; num2 <= '3' ; num2++)
-{
-/** Digit Three Loop */
-for (num3 = '0' ; num3 <= '5' ; num3++)
-{
-/** Digit Four Loop */
-for (num4 = '0' ; num4 <= '9' ; num4++)
-{
-/** Printing Agruements */
-_putchar(num1);
-_putchar(num2);
-_putchar(':');
-_putchar(num3);
-_putchar(num4);
-_putchar('\n');
-}
-}
-}
-}
+	int num1, num2;
 
+	for (num1 = 0 ; num1 <= 23 ; num1++)  /** Digit One Loop */
+	{
+		for (num2 = 0 ; num2 <= 59 ; num2++)
+		{
+			_putchar('0' + (num1 / 10)); /** Printing Agruements */
+			_putchar('0' + (num1 % 10));
+			_putchar(':');
+			_putchar('0' + (num2 / 10));
+			_putchar('0' + (num2 % 10));
+			_putchar('\n');
+		}
+	}
 }
