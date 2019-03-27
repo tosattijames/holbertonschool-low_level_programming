16fc38904fd78a45e6a42dc9c5d09a4e9295d78c
tosattijames <703@holbertonschool.com>
2019-03-27 03:01:52 +0000

    wip

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -24,7 +24,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		counter++;
 	}
 
-        if (index > counter)
+	if (index > counter)
 		return (NULL);
 
 	return (current);
