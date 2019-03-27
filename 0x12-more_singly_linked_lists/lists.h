6a8dcb75f63d47c55c37f1bf09836db2f2e95193
tosattijames <703@holbertonschool.com>
2019-03-27 13:00:27 +0000

    corrected

--- 0x12-more_singly_linked_lists/lists.h
+++ 0x12-more_singly_linked_lists/lists.h
@@ -1,9 +1,11 @@
+
 #ifndef _LIST_H_
 #define _LIST_H_
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
+/* include <stdarg.h> */
+
 
 /**
  * struct listint_s - singly linked list
@@ -27,6 +29,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
+int sum_listint(listint_t *head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
