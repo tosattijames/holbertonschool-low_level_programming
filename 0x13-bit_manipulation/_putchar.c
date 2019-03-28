6fe2aa4d01fc59fdaf2b09caeb3f9727de34b0a7
James Tosatti <tosattijames@gmail.com>
2019-03-28 22:40:27 +0000

    putchar func

---
+++ 0x13-bit_manipulation/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putcchar - prints  character c to stdout
+ * @c: character to print
+ * Return: On success 1.
+ * On error, -1 is returned
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
