4fbe729bdccfbc52f508dcb352d09b57feafdd42
James Tosatti <tosattijames@gmail.com>
2019-03-31 23:56:30 +0000

    line fixed

---
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+
+#include "holberton.h"
+
+/**
+ * print_line - prints a line
+ * @n: input number
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	int space;
+
+	if (n <= 0)
+		_putchar('\n');
+
+	for (space = 0; space < n; space++)
+	{
+		_putchar('_');
+	}
+
+	_putchar('\n');
+}
