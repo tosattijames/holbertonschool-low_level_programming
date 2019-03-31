84e03d3086a0d6ab6deb401dcb7abd524d56399e
tosattijames <703@holbertonschool.com>
2019-03-31 23:31:13 +0000

    source

---
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14, followed by a new
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int rows, num1;
+
+	for (rows = 0; rows <= 14; rows++)
+	{
+		for (num1 = 0; num1 <= 14; num1++)
+		{
+			if (num1 > 9)
+			{
+				_putchar ('0' +(num1 / 10));
+			}
+			_putchar('0' +(num1 % 10));
+		}
+		_putchar('\n');
+	}
+
+}
