913678b126e13d6e490649086965aa5df2dbe55c
tosattijames <703@holbertonschool.com>
2019-03-31 23:00:53 +0000

    source

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - prints a diagonal line
+ * @n: input number
+ * Return: Always 0.
+ */
+
+void print_diagonal(int n)
+{
+	int space, row;
+
+	if (n <= 0)
+		_putchar('\n');
+
+	for (row = 0; row < n; row++)
+	{
+		for (space = 0; space < row; space++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
