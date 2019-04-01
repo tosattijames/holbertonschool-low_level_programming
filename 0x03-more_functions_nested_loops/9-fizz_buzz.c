85bb46f21225ffa6a063d46354bd267a66c66644
James Tosatti <tosattijames@gmail.com>
2019-04-01 03:25:41 +0000

    fizz buzz

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Prints Fizz Buzz
+ * Description: Prints 1 to 100. Multiples of three print Fizz
+ * Multiples of five print Buzz. Multiples of both three
+ * and five print FizzBuzz
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x < 100; x++)
+	{
+		if (((x % 5) == 0) && ((x % 3) == 0))
+		{
+			printf("FizzBuzz");
+		}
+
+		else if ((x % 5) == 0)
+		{
+			printf("Buzz");
+		}
+
+		else if ((x % 3) == 0)
+		{
+			printf("Fizz");
+		}
+
+		else
+		{
+			printf("%d", x);
+		}
+
+		if (x != 100)
+		{
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return 0;
+}
