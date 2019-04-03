757f22f1398d0223978b67d6758626e07068ee1c
James Tosatti <tosattijames@gmail.com>
2019-04-03 01:50:46 +0000

    header

--- 0x04-pointers_arrays_strings/holberton.h
+++ 0x04-pointers_arrays_strings/holberton.h
@@ -1,24 +1,10 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include <stdio.h>
+
 int _putchar(int c); 
 
-/* 0x02*/
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-
-/*0x03*/
-
-/*0x04*/
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
