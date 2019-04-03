42a902ac1ac481ef9f06340f1e69e7e3df54bc77
James Tosatti <tosattijames@gmail.com>
2019-04-03 00:26:06 +0000

    reverse string

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -9,9 +9,10 @@ void print_rev(char *s)
 	int strLen;
 
 	for (strLen = 0; s[strLen] != 0; strLen++)
-		;
 	strLen--;
+
 	for (; strLen >= 0; strLen--)
 		_putchar(s[strLen]);
+
 	_putchar('\n');
 }
