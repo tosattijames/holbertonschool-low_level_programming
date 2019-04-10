57753f8d34b5ee1474fa5810711779f607a51eeb
tosattijames <703@holbertonschool.com>
2019-04-10 01:10:54 +0000

    betty format

--- 0x07-recursion/3-factorial.c
+++ 0x07-recursion/3-factorial.c
@@ -6,16 +6,18 @@
  */
 int factorial(int n)
 {
-if (n >= 1)
-{
-return (n * factorial(n - 1));
-}
-else if (n < 0)
-{
-return (-1);
-}
-else
-{
-return (1);
-}
+	if (n >= 1)
+	{
+		return (n * factorial(n - 1));
+	}
+
+	else if (n < 0)
+	{
+		return (-1);
+	}
+
+	else
+	{
+		return (1);
+	}
 }
