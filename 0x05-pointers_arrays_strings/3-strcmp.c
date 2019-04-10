e06603acb0729b56901377f330fca66d3040cf70
tosattijames <703@holbertonschool.com>
2019-04-10 14:09:21 +0000

    compare string

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -4,23 +4,19 @@
  *_strcmp - compare two strings
  *@s1: string one
  *@s2: string two
- * Return: the return is an int
+ * Return: result if true, ) if false
  */
+
 int _strcmp(char *s1, char *s2)
 {
-	int result = 0;
-
-	if (s1 != NULL || s2 != NULL)
+	while (*s1)
 	{
-		result = *s1 - *s2;
-
-		if (result != 0)
+		if (*s1 != *s2)
 		{
-
+			break;
 		}
-
 		s1++;
 		s2++;
 	}
-	return (result);
+	return (*s1 - *s2);
 }
