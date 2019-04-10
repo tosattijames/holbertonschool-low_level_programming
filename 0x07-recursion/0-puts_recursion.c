3e697262552ec94af006340ddb3c5852bd5bc072
tosattijames <703@holbertonschool.com>
2019-04-10 00:31:18 +0000

    corrected output

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -2,15 +2,17 @@
 /**
  * _puts_recursion - prints a string
  *@s: string to print
- *Return: print string to output
+ *Return: void
  */
 void _puts_recursion(char *s)
 {
-if (*s == '\0')
-{
-_putchar('\n');
-return;
-}
-_putchar(*s);
-s++;
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+
+	else
+		_putchar('\n');
 }
